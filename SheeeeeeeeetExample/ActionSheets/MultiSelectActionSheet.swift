1f3807fb68a0ff1e6c42b10e742cbc04de06d679
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2018-03-29 08:05:41 +0200

    Add selectAll and deselectAll to multiselect title

--- SheeeeeeeeetExample/ActionSheets/MultiSelectActionSheet.swift
+++ SheeeeeeeeetExample/ActionSheets/MultiSelectActionSheet.swift
@@ -13,6 +13,25 @@ class MultiSelectActionSheet: ActionSheet {
     init(options: [FoodOption], preselected: [FoodOption], action: @escaping ([ActionSheetItem]) -> ()) {
         let items = MultiSelectActionSheet.items(for: options, preselected: preselected)
         super.init(items: items) { sheet, item in
+            
+            if item is ActionSheetTitle {
+                
+                var selectAction: Bool
+
+                let selectItems = sheet.items.flatMap { $0 as? ActionSheetSelectItem }
+                if (selectItems.filter { $0.isSelected }).count > 0 {
+                    selectAction = false
+                    item.subtitle = "Select all"
+                } else {
+                    selectAction = true
+                    item.subtitle = "Deselect all"
+                }
+
+                for itemToUpdate in sheet.items {
+                    (itemToUpdate as? ActionSheetSelectItem)?.isSelected = selectAction
+                }
+            }
+            
             guard item is ActionSheetOkButton else { return }
             let selectItems = sheet.items.flatMap { $0 as? ActionSheetSelectItem }
             action(selectItems.filter { $0.isSelected })
@@ -28,7 +47,7 @@ fileprivate extension MultiSelectActionSheet {
     
     static func items(for options: [FoodOption], preselected: [FoodOption]) -> [ActionSheetItem] {
         var items = options.map { $0.selectItem(isSelected: preselected.contains($0)) }
-        items.insert(createTitleItem(title: standardTitle), at: 0)
+        items.insert(createTitleItem(title: standardTitle, subtitle: preselected.count > 0 ? "Deselect all" : "Select all"), at: 0)
         items.append(createOkButton())
         items.append(createCancelButton())
         return items
