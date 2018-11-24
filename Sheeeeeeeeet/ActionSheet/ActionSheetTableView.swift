8a2d4548e96b412433249f0247326dc05c6b3515
Daniel Saidi <daniel.saidi@gmail.com>
2018-11-24 22:11:49 +0100

    Add custom table view subclass and background color appearance

---
+++ Sheeeeeeeeet/ActionSheet/ActionSheetTableView.swift
@@ -0,0 +1,20 @@
+//
+//  ActionSheetTableView.swift
+//  Sheeeeeeeeet
+//
+//  Created by Daniel Saidi on 2018-11-24.
+//  Copyright © 2018 Daniel Saidi. All rights reserved.
+//
+
+/*
+ 
+ This class can be used to apply a custom appearance to your
+ action sheets, e.g. with `ActionSheetTableView.appearance()`
+ which is the normal iOS way of styling components. However,
+ you should probably use `ActionSheetAppearance` if possible.
+ 
+ */
+
+import UIKit
+
+open class ActionSheetTableView: UITableView {}
