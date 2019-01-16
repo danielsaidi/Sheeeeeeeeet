7458bfcafb72a07ba4f325296e8bba827219cd3c
Daniel Saidi <daniel.saidi@gmail.com>
2019-01-16 16:20:28 +0100

    Add scroll extension to action sheet

---
+++ Sheeeeeeeeet/ActionSheet/ActionSheet+Scroll.swift
@@ -0,0 +1,30 @@
+//
+//  ActionSheet+Scroll.swift
+//  Sheeeeeeeeet
+//
+//  Created by Sebastian Bolling on 2019-01-16.
+//  Copyright © 2019 Sebastian Bolling. All rights reserved.
+//
+
+/*
+ 
+ This extension can be used to scroll to the first selected
+ item in scrollable action sheets that have many items.
+ 
+ */
+
+import UIKit
+
+public extension ActionSheet {
+
+    func scrollToFirstSelectedItem(at position: UITableView.ScrollPosition = .middle, animated: Bool = false) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) { [weak self] in
+            guard
+                let items = (self?.items.compactMap { $0 as? ActionSheetSelectItem }),
+                let index = (items.index { $0.isSelected == true })
+                else { return }
+            let path = IndexPath(row: index, section: 0)
+            self?.itemsTableView?.scrollToRow(at: path, at: position, animated: animated)
+        }
+    }
+}
