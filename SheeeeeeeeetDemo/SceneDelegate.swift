4312cacac7febbfd9d87c3922fe9daae68532f6c
Daniel Saidi <daniel.saidi@gmail.com>
2019-08-21 08:22:55 +0200

    Lower dependency target of demo app

--- SheeeeeeeeetDemo/SceneDelegate.swift
+++ SheeeeeeeeetDemo/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    @available(iOS 13.0, *)
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         ActionSheet.applyAppearance(.demo)
         guard let _ = (scene as? UIWindowScene) else { return }
