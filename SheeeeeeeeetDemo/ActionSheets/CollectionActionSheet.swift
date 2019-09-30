aabeb9da84ec83afd717c433d822557e668134b6
Daniel Saidi <daniel.saidi@gmail.com>
2019-09-30 11:30:07 +0200

    Cleanup collection action sheet

--- SheeeeeeeeetDemo/ActionSheets/CollectionActionSheet.swift
+++ SheeeeeeeeetDemo/ActionSheets/CollectionActionSheet.swift
@@ -9,34 +9,15 @@
 import Sheeeeeeeeet
 
 /**
- This action sheet takes a `Menu` and adjusts any collection
- items it may have, so that the generated action sheet items
- correctly update the action sheet.
- 
- `IMPORTANT` Action sheets with `ActionSheetCollectionItem`s
- that are mapped from `CollectionItem`s must adjust the item
- select action, if it wants to be able to listen for taps in
- the collection view. You don't have to do it in this way if
- you create an `ActionSheetCollectionItem` directly from the
- action sheet, since the sheet can then refer to itself when
- setting up the select action.
+ This action sheet takes a `CollectionMenu` and modifies its
+ select action to fit the collection view.
  */
 class CollectionActionSheet: ActionSheet {
     
-    typealias Menu = CollectionMenu
-    
-    convenience init(menu: Menu, action: @escaping ([Menu.Cell.Item]) -> ()) {
+    convenience init(menu: CollectionMenu, action: @escaping ([CollectionMenu.Cell.Item]) -> ()) {
         self.init(menu: menu) { _, item in
             guard item is OkButton else { return }
             action(menu.selectedItems)
-        }
-        
-        let sectionTitle = items.compactMap { $0 as? SectionTitle }.first
-        let collectionItem = items.compactMap { $0 as? CollectionItem }.first
-        let selectionAction = collectionItem?.itemSelectionAction
-        collectionItem?.itemSelectionAction = { [weak self] cell, index in
-            selectionAction?(cell, index)
-            //self?.reloadData()
         }
     }
 }
