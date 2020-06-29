aba10d86ca232490d0fa6147d7dd4a7b1af72c0e
Daniel Saidi <daniel.saidi@gmail.com>
2020-06-29 08:58:04 +0200

    Update external dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,9 +14,9 @@ let package = Package(
             targets: ["Sheeeeeeeeet"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.7")),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.3.2"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.3.3"),
     ],
     targets: [
         .target(
