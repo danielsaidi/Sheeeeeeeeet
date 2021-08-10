3031d42c94a7f05c096a6c2704405251e9620cff
Daniel Saidi <daniel.saidi@gmail.com>
2021-08-10 14:51:28 +0200

    Update standard presenter to use window from vc instead of the shared application's keyWindow

--- Sources/Sheeeeeeeeet/ActionSheet/Presenters/ActionSheetStandardPresenter.swift
+++ Sources/Sheeeeeeeeet/ActionSheet/Presenters/ActionSheetStandardPresenter.swift
@@ -122,14 +122,18 @@ open class ActionSheetStandardPresenter: ActionSheetPresenterBase {
 
 extension ActionSheetStandardPresenter {
     
-    func addActionSheetToKeyWindow(_ sheet: ActionSheet) {
-        let window = UIApplication.shared.keyWindow
-        sheet.view.frame = window?.bounds ?? .zero
-        window?.addSubview(sheet.view)
+    func addActionSheetToKeyWindow(
+        _ sheet: ActionSheet,
+        for viewController: UIViewController) {
+        guard let window = viewController.view.window else { return }
+        sheet.view.frame = window.bounds
+        window.addSubview(sheet.view)
     }
     
-    func addActionSheet(_ sheet: ActionSheet, to vc: UIViewController) {
-        if presentationStyle == .keyWindow { return addActionSheetToKeyWindow(sheet) }
+    func addActionSheet(
+        _ sheet: ActionSheet,
+        to vc: UIViewController) {
+        if presentationStyle == .keyWindow { return addActionSheetToKeyWindow(sheet, for: vc) }
         let view = vc.view
         sheet.view.frame = view?.bounds ?? .zero
         view?.addSubview(sheet.view)
